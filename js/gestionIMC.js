e7ece8573e2a05c1ffbefdb485e337503e3cd50e
DebasDiez84 <49246065+DebasDiez84@users.noreply.github.com>
2020-03-14 23:28:27 -0500

    correccion de algunos errores, sin poder funcionar el LocalStorage

--- js/gestionIMC.js
+++ js/gestionIMC.js
@@ -27,6 +27,7 @@ function obtenerValores() {
     
     //Aqui se hace el arreglo del usuario
     let miUsuario2 = { tipoDI, id, nombre, apellidos, correo, peso, estatura, convertidoIMC }
+    console.log("Se creo el usuario "+id)
     //localStorage.setItem('usuarios',JSON.stringify(miUsuario2))
     return miUsuario2
 }
@@ -60,6 +61,8 @@ function crearUsuario() {
 function eliminarUsuario(index) {
     usuarios.splice(index, 1)
     listarUsuarios()
+    alert("Se elimino el usuario ")
+    console.log("Se elimino el usuario ")
 }
 
 function cargarInformacion(index) {
@@ -75,6 +78,8 @@ function cargarInformacion(index) {
 
     document.getElementById("btnCrearUsuario").style.display = "none"
     document.getElementById("btnModificarUsuario").style.display = "inline"
+
+    console.log("Se va a modificar el usuario "+ usuario.id)
 }
 
 
@@ -97,6 +102,7 @@ function modificarUsuario() {
     usuarios.splice(usuarioTemporal, 1, usuarioActualizado)
    
     listarUsuarios(limpiarFormulario)
+    console.log("Se modifico el usuario correctamente")
 }
 
 function consultar(index){
@@ -110,26 +116,26 @@ function consultar(index){
     estaturaMetros = consultaEstatura/100
     indiceMasaCorporal = consultaPeso/(estaturaMetros*estaturaMetros) 
     convertidoIMC = indiceMasaCorporal.toFixed(1)
-    console.log("este es el IMC del consultado "+convertidoIMC)
+    console.log("este es el IMC "+convertidoIMC+ "del consultado "+ id)
 
     if(convertidoIMC<18.5){
         alert("Bajo peso")
-        console.log("quedo en bajo peso")
+        //console.log("quedo en bajo peso")
     }else{
-        console.log("entro al condicional")
+        //console.log("entro al condicional")
         if (30>convertidoIMC && convertidoIMC>=18.5){
             alert("Peso normal")
-            console.log("quedo en peso normal")
+            //console.log("quedo en peso normal")
         }else{
             console.log("entro al segundo condicional")
             if(35>convertidoIMC && convertidoIMC>=30){
                 alert("Sobre Peso")
-                console.log("quedo en sobre peso")
+                //console.log("quedo en sobre peso")
             }else{
-                console.log("entro al tercer condicional")
+                //console.log("entro al tercer condicional")
                 if(35<=convertidoIMC){
                     alert("obesidad")
-                    console.log("quedo en obeso")
+                    //console.log("quedo en obeso")
                 }
             }
         }
@@ -140,7 +146,7 @@ function consultar(index){
 
 
 
-function listarUsuarios(callback) {
+function listarUsuarios() {
     //localStorage.getItem('usuarios')
     let lista = document.getElementById("listaUsuarios")
     let data = ""
@@ -156,11 +162,11 @@ function listarUsuarios(callback) {
         data += `<td>${miUsuario2.convertidoIMC} </td>`
         data += `<td><button type="button" onclick="cargarInformacion(${i})" class="btn btn-primary btn-sm">Modificar</button> </td>`
         data += '<td><button type="button" onclick="eliminarUsuario(' + i + ')" class="btn btn-primary btn-sm">Eliminar</button> </td>'
-        data += `<td><button type="button" onclick="consultar(${i})" class="btn btn-primary btn-sm">VerEstado</button> </td>`
+        data += `<td><button type="button" onclick="consultar(${i})" class="btn btn-primary btn-sm">Ver Estado</button> </td>`
         data += "</tr>"
     }
     lista.innerHTML = data
-    callback()
+    
 }
 
 // Llamado a la función
